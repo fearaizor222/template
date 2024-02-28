cbedfac58523e7dbf2c55f7d2ffd152cf7d42b79
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2024-02-28 20:11:43 +0700

    Refactor code for improved performance

---
+++ src/main/java/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping("welcome")
+    public String welcome() {
+        System.out.println("WelcomeController: welcome()");
+        return "welcome";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome page</title>
+    <link rel="stylesheet" href="resources/welcomepage.css">
+</head>
+<body>
+    <h1>you landed on the welcome page</h1>
+</body>
+</html>
